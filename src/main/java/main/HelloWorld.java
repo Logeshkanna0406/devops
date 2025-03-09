0f7b15d60f1ef14089d5afc0e5790ecd5168bcee
Logesh Kanna <logesh@example.com>
2025-03-09 18:54:02 +0530

    Add target directory with compiled class files

--- src/main/java/main/HelloWorld.java
+++ src/main/java/main/HelloWorld.java
@@ -1,7 +1,20 @@
 package main;
 
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // Accept user input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Perform a simple calculation
+        int result = number * 2;
+        System.out.println("Double of " + number + " is " + result);
+        
+        scanner.close();
     }
 }
